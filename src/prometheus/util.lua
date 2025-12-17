730a60cf2c074b8b31bb571a259bd766e110ee38
idkjustarandomdudeherenothingtosee <promcpegamer99@gmail.com>
2025-12-18 01:05:21 +0300

    Update util.lua

--- src/prometheus/util.lua
+++ src/prometheus/util.lua
@@ -25,39 +25,7 @@ local function unlookupify(tb)
 end
 
 local function escape(str)
-	return str:gsub(".", function(char)
-		if char:match("[^ %-~\n\t\a\b\v\r\"\']") then -- Check if non Printable ASCII Character
-			return string.format("\\%03d", string.byte(char))
-		end
-		if(char == "\\") then
-			return "\\\\";
-		end
-		if(char == "\n") then
-			return "\\n";
-		end
-		if(char == "\r") then
-			return "\\r";
-		end
-		if(char == "\t") then
-			return "\\t";
-		end
-		if(char == "\a") then
-			return "\\a";
-		end
-		if(char == "\b") then
-			return "\\b";
-		end
-		if(char == "\v") then
-			return "\\v";
-		end
-		if(char == "\"") then
-			return "\\\"";
-		end
-		if(char == "\'") then
-			return "\\\'";
-		end
-		return char;
-	end)
+    return str
 end
 
 local function chararray(str)
